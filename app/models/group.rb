a519b36b906d68e28a0c5305eaec3062d282d751
svenka8 <sriram.venkat@chicagobooth.edu>
2020-11-30 18:29:37 +0000

    groups direct and indirect done.   #direct   has_many(:grouplists, { :class_name => "Grouplist", :foreign_key => "group_id", :dependent => :destroy })
      #indirect
    
      has_many(:users, { :through => :grouplists, :source => :user })

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,4 +8,12 @@
 #  updated_at :datetime         not null
 #
 class Group < ApplicationRecord
+  #direct
+  has_many(:grouplists, { :class_name => "Grouplist", :foreign_key => "group_id", :dependent => :destroy })
+
+  #indirect
+
+  has_many(:users, { :through => :grouplists, :source => :user })
+
+  
 end
