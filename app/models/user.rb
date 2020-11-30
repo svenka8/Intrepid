49b069f51f55310608bc12ae96b5c56228558cd2
svenka8 <sriram.venkat@chicagobooth.edu>
2020-11-30 18:27:15 +0000

    direct and indirect associations done. has_many(:courses, { :through => :courselists, :source => :course })
    has_many(:courses, { :through => :courselists, :source => :course })
    
    has_many(:courses, { :through => :courselists, :source => :course })
    
    **indirect**
    
    has_many(:courses, { :through => :courselists, :source => :course })
    
    has_many(:courses, { :through => :courselists, :source => :course })

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,14 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+#direct associations
+  has_many(:grouplists, { :class_name => "Grouplist", :foreign_key => "user_id", :dependent => :destroy })
+
+  has_many(:courselists, { :class_name => "Courselist", :foreign_key => "user_id", :dependent => :destroy })
+
+  has_many(:answers, { :class_name => "Answer", :foreign_key => "user_id", :dependent => :destroy })
+#indirect associations
+  has_many(:groups, { :through => :grouplists, :source => :group })
+
+  has_many(:courses, { :through => :courselists, :source => :course })
 end
