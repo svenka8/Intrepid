920e4db7d0034990019bca65878c13a53e2eaf12
svenka8 <sriram.venkat@chicagobooth.edu>
2020-11-30 18:28:22 +0000

    generate and migrate groups rails generate draft:resource group name:string

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,54 @@
+class GroupsController < ApplicationController
+  def index
+    matching_groups = Group.all
+
+    @list_of_groups = matching_groups.order({ :created_at => :desc })
+
+    render({ :template => "groups/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_groups = Group.where({ :id => the_id })
+
+    @the_group = matching_groups.at(0)
+
+    render({ :template => "groups/show.html.erb" })
+  end
+
+  def create
+    the_group = Group.new
+    the_group.name = params.fetch("query_name")
+
+    if the_group.valid?
+      the_group.save
+      redirect_to("/groups", { :notice => "Group created successfully." })
+    else
+      redirect_to("/groups", { :notice => "Group failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_group = Group.where({ :id => the_id }).at(0)
+
+    the_group.name = params.fetch("query_name")
+
+    if the_group.valid?
+      the_group.save
+      redirect_to("/groups/#{the_group.id}", { :notice => "Group updated successfully."} )
+    else
+      redirect_to("/groups/#{the_group.id}", { :alert => "Group failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_group = Group.where({ :id => the_id }).at(0)
+
+    the_group.destroy
+
+    redirect_to("/groups", { :notice => "Group deleted successfully."} )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,25 @@ Rails.application.routes.draw do
 
 
 
+  # Routes for the Group resource:
+
+  # CREATE
+  post("/insert_group", { :controller => "groups", :action => "create" })
+          
+  # READ
+  get("/groups", { :controller => "groups", :action => "index" })
+  
+  get("/groups/:path_id", { :controller => "groups", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_group/:path_id", { :controller => "groups", :action => "update" })
+  
+  # DELETE
+  get("/delete_group/:path_id", { :controller => "groups", :action => "destroy" })
+
+  #------------------------------
+
   # Routes for the User account:
 
   # SIGN UP FORM
