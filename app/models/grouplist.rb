c4f18a2f53c10ff1852ae01231303069541c924a
svenka8 <sriram.venkat@chicagobooth.edu>
2020-11-30 18:32:57 +0000

    grouplist direct association done (join table)  # direct   belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id" })
      belongs_to(:group, { :required => false, :class_name => "Group", :foreign_key => "group_id" })

--- app/models/grouplist.rb
+++ app/models/grouplist.rb
@@ -9,4 +9,10 @@
 #  user_id    :integer
 #
 class Grouplist < ApplicationRecord
+ # direct
+  belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id" })
+
+  belongs_to(:group, { :required => false, :class_name => "Group", :foreign_key => "group_id" })
+
+  
 end
